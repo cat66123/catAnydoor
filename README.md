ca0607b4882f515d1c66b08bf5a3603e36ed1c7b
cat66123 <326700510@qq.com>
2019-09-27 19:14:49 +0800

    config

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # catAnydoor
+
 NodeJS static Web server
+
+# note
+
+全局安装 supervisor,可以直接刷新看到效果
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.error(1);
+const http = require("http");
+const chalk = require("chalk");
+const config = require("./config/defaultConfig");
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.write(`<htm><body>Hello cat66</body></htm>`);
+  res.end();
+});
+server.listen(config.port, config.hostname, () => {
+  const addr = `http://${config.hostname}:${config.port}`;
+  console.info(`Server started at ${chalk.green(addr)}`);
+});
